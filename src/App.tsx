1013b9f48770f97e64a00edca142f5cc785fa2c3
mh-morowati <162552049+mh-morowati@users.noreply.github.com>
2024-11-04 10:11:02 +0330

    install tailwind & create home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
+import { FaUser } from "react-icons/fa6";
 
 
 function App() {
   return (
     <div>
-      
+      <nav className="bg-gray-800">
+        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+          <div className="relative flex h-16 items-center justify-between">
+            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+
+              <div className="hidden sm:ml-6 sm:block">
+
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* <!-- Mobile menu, show/hide based on menu state. --> */}
+        <div className="sm:hidden" id="mobile-menu">
+
+        </div>
+      </nav>
+
+      <div>
+      <FaUser className="w-96 h-96 text-zinc-200 absolute right-1 bottom-36"/>
+        <h1 className="text-5xl text-slate-950 bottom-64 absolute left-20">Hi. i'm Muhammad</h1>
+      </div>
     </div>
   );
 }
