c923e194e8fa12aba16438966bd23f019b1b4c85
mh-morowati <162552049+mh-morowati@users.noreply.github.com>
2025-03-09 09:05:36 -1200

    feat: add my resume

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,3 @@
-import { FaUser } from "react-icons/fa6";
-import { SiGithub } from "react-icons/si";
-import { BsLinkedin } from "react-icons/bs";
-import { SiGmail } from "react-icons/si";
 
 
 
@@ -9,18 +5,163 @@ function Home() {
   return (
     <div>
       <div className="flex p-5">
-        <a className="mr-3" target="_blank" href="https://github.com/mh-morowati" rel="noreferrer"><SiGithub className="w-7 h-7 inline-block" /></a>
+        <a className="mr-3" target="_blank" href="https://github.com/mh-morowati" rel="noreferrer"> Github </a>
         <a className="mr-3" href="https://www.linkedin.com/in/muhammad-morowati-b12507307/" target="_blank" rel="noopener noreferrer">
-          <BsLinkedin className="w-7 h-7 inline-block text-[#0075b4]" /></a>
-          <a className="mr-3" href="mailto:muhammadmorowati@gmail.com"><SiGmail className="w-7 h-7 inline-block text-[#ed4739]" /></a>
+          Linkedin </a>
+          <a className="mr-3" href="mailto:muhammadmorowati@gmail.com"> Gmail </a>
           <a className="mr-3 text-sm font-medium p-1 bg-slate-200 rounded" href="tel:+989187139586">+989187139586</a>
       </div>
+      <div className="flex float-right border pl-4">
+        <div className="w-[815px]">
+    <div>
+      <div className=" ">
+      <h1 className="text-lg tracking-[2px]
+"><span className="font-light">MOHAMMAD</span> &nbsp;<span className="font-bold">MOROWATI</span></h1>
+        <h3 className=" text-zinc-400 font-semibold tracking-[2px]">FRONT-END DEVELOPER</h3>
+        <div className="space-x-5 flex">
+         <p className="tracking-wide">
+        918-713-9586
+      </p>
+      <p className="">
+        Tehran
+      </p>
+      
+       <p className="">
+        <a href="https://www.linkedin.com/in/muhammad-morowati-b12507307/">
+          Linkedin
+        </a>
+      </p>
+       <p className="">
+        <a href="https://github.com/mh-morowati">
+        Github
+        </a>
+      </p>
+       <p className="">
+        <a href="https://t.me/mhmorowati">
+        Telegram
+        </a>
+      </p> 
+       <p className="">
+       
+end of service
+          </p>
+           <p className="">
+        2000
+          </p>
+         
+      </div>
+      </div>
+       <hr className="my-1.5"/>
       <div>
-        <FaUser className="w-96 h-96 text-zinc-200 absolute right-1 bottom-36" />
-        <div className="bottom-60 absolute left-20 max-sm:top-60">
-          <h1 className="text-5xl text-slate-950 max-sm:text-2xl">Hi. i'm Muhammad .</h1>
-          <h3 className="mt-2 text-slate-950"> I'm Frontend Developer with React and Next.js</h3>
-        </div>
+        <h1 className="text-xl tracking-[3px] text-zinc-900 ml-1">ABOUT ME</h1>
+        <p className="text-sm text-zinc-800 ">
+          Passionate Frontend Developer with 2 years of experience crafting user-centric experiences and high-performance web solutions
+     using JavaScript. I leverage clean code practices and a strong understanding of the business domain to deliver impactful projects.
+        </p>
+        <hr className="my-1.5" />
+      </div>
+
+
+       <div>
+        <h1 className="text-xl tracking-[3px] text-zinc-900 ml-1 mb-2">Experience</h1>
+        <h3 className="font-bold">p30web</h3>
+        <p><span className="text-zinc-700 text-sm">Aban1403 - current</span></p>
+        <p className="text-sm text-zinc-800 ">
+          Developed and contributed to the website <a
+            className="text-blue-600 underline underline-offset-4" href="https://expertiew.com/">Expertiew </a>
+          
+        </p>
+        <ol className="list-disc pl-6">
+          <li>using Next.js, React, Typescript, google auth, Next ui, react query, react hook form</li>
+          <li>Implemented Google authentication for user login.</li>
+          <li>Designed and developed a sign-in form for interviewers.</li>
+          <li>Created the booking page and other sections of the website.</li>
+          <li>Worked closely with the backend team to integrate features and ensure seamless functionality.</li>
+        </ol>
+        <hr className="my-1.5"/>
+      </div>
+    </div>
+     <div className="">
+        <h1 className="text-2xl tracking-[3px] text-zinc-900 mb-1 ml-1">SKILLS</h1>
+      <div className="flex space-x-20 pl-6">
+          <ol className="list-disc">
+          <li>Next</li>
+          <li>React</li>
+          <li>Typescript</li>
+        </ol>
+        <ol className="list-disc">
+            <li>Javascript</li>
+          <li>Zustand</li>
+          <li>RESTful Api</li>
+        </ol>
+        <ol className="list-disc">
+
+          <li>Axios</li>
+          <li>Sass/Less</li>
+          <li>Tailwind</li>
+         </ol>
+        <ol className="list-disc">
+          <li>Next ui</li>
+         <li>Git</li>
+          <li>Figma</li>
+          
+        </ol>
+        <ol className="list-disc">
+          <li>Trello</li>
+        </ol>
+      </div>
+       
+    </div>
+
+    <div>
+       <hr className="my-1.5"/>
+        <h1 className="text-2xl tracking-[3px] text-zinc-900 ml-1 mb-1">PROJECTS</h1>
+        <h3 className="font-bold">Squid game</h3>
+        <p className="text-sm text-zinc-800 ">
+         Developed an interactive "red light, green light" game using:</p>
+      <ol className="list-disc pl-6">
+        <li>Next, react, typescript, next ui, framer motion, tailwind</li>
+          <li>Implemented smooth animations with Framer Motion.</li>
+          <li>Designed and optimized custom hooks for better game logic.</li>
+        <li>Ensured full responsiveness across all screen sizes and frames.</li>
+        <p className="flex space-x-6">
+          <a className="text-blue-600 flex" href="https://squid-game-chi.vercel.app/">
+             Vercel
+          </a>
+          <a className="text-blue-600 flex" href="https://github.com/mh-morowati/squid_game">
+            github 
+          </a>
+        </p>
+      </ol>
+      
+       <h3 className="font-bold mt-1.5">Cryptocurrency Price Tracker – ArzDigital</h3>
+        <p className="text-sm text-zinc-800 ">
+         Developed a cryptocurrency price tracking website using:</p>
+      <ol className="list-disc pl-6">
+        <li>Next, react, typescript, axios, react query</li>
+          <li>Designed and implemented real-time price updates.</li>
+          <li>Optimized performance for smooth user experience.</li>
+          <li>Added a reliable search functionality for cryptocurrencies.</li>
+        <li className="flex space-x-6" ><a className="text-blue-600 flex" href="https://arzdigital.vercel.app/">
+         Vercel </a>
+        <a className="text-blue-600 flex" href="https://github.com/mh-morowati/arzdigital">
+           github </a></li>
+        </ol>
+       
+        <h3 className="font-bold mt-1.5">E-Commerce Project – Digikala Clone</h3>
+        <p className="text-sm text-zinc-800 ">
+         Developed a Digikala-like online store using Next.js,Typescript ,NextUI, and React Query.</p>
+        <ol className="list-disc pl-6">
+          <li>Implemented a authentication system using Next Auth and React Query Provider.</li>
+          <li>Integrated an API for fetching and displaying product data.</li>
+        <li>Used a JSON-based approach for city selection.</li>
+        <li>Enabled product selection and shopping cart functionality with local storage.</li>
+        <li>Optimized UI and responsiveness for a seamless shopping experience.</li>
+        <li ><a className="text-blue-600 flex" href="https://github.com/mh-morowati/digikala-clone">
+           github </a></li>
+        </ol>
+      </div>
+  </div>
       </div>
     </div>
   );
